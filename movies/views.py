cc9644aad76d9df8aa66743ba6c40db1b1b2b37f
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-09-29 16:20:36 -0400

    create movie and still viewsets

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MovieSerializer, StillSerializer
+from .models import Movie, Still
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class StillViewSet(viewsets.ModelViewSet):
+    queryset = Still.objects.all()
+    serializer_class = StillSerializer
