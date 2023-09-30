42a02f96c59787cc337fbc163ce6e7bd1f60e33e
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-09-30 00:39:33 -0400

    update serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Movie, Still
 
 
-class MovieSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Movie
-        fields = [f.name for f in Movie._meta.fields] + ['stills']
-
-
-class StillSerializer(serializers.HyperlinkedModelSerializer):
+class StillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Still
         fields = '__all__'
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    stills = StillSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = [f.name for f in Movie._meta.fields] + ['stills']
