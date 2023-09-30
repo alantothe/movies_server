a183a713770300a216afeb33d498796aef7c311a
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-09-30 00:57:22 -0400

    set movies, still view set permissions: GET requests only

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from .serializers import MovieSerializer, StillSerializer
 from .models import Movie, Still
+from .permissions import GETRequestsOnly
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    permission_classes = [GETRequestsOnly]
 
 
 class StillViewSet(viewsets.ModelViewSet):
     queryset = Still.objects.all()
     serializer_class = StillSerializer
+    permission_classes = [GETRequestsOnly]
