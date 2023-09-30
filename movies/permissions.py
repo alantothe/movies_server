e2cfd8e0e16b8bde688008d5584af2bfc515e8e3
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-09-30 01:13:39 -0400

    replace view sets custom permissions with read only model viewset

--- movies/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions
-
-
-class GETRequestsOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method == 'GET':
-            return True
--- movies/views.py
+++ movies/views.py
@@ -1,16 +1,13 @@
 from rest_framework import viewsets
 from .serializers import MovieSerializer, StillSerializer
 from .models import Movie, Still
-from .permissions import GETRequestsOnly
 
 
-class MovieViewSet(viewsets.ModelViewSet):
+class MovieViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = [GETRequestsOnly]
 
 
-class StillViewSet(viewsets.ModelViewSet):
+class StillViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Still.objects.all()
     serializer_class = StillSerializer
-    permission_classes = [GETRequestsOnly]
