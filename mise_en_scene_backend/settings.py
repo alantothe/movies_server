f9d9362438a9b64326b6ae78c423c04ae272e183
alantothe <104442054+alantothe@users.noreply.github.com>
2023-10-01 02:45:52 -0400

    update ALLOW_HOSTS

--- mise_en_scene_backend/settings.py
+++ mise_en_scene_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ga+cb4#-yj%03ts_%rqs2)pfobeoms)=em#8&grbe=(7bpdqc$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
