f1ddbfc6281947e5400240ba3a3065e9664f3317
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-10-03 17:24:59 -0400

    movie model: change contry, genre, driector fields to CharField

--- movies/models.py
+++ movies/models.py
@@ -5,15 +5,14 @@ class Movie(models.Model):
     imdb_id = models.CharField(max_length=20, primary_key=True)
     title = models.CharField(max_length=255)
     date_released = models.DateField()
-    genre = models.JSONField()
+    genre = models.CharField(max_length=1000)
     rating = models.CharField(max_length=10)
-    director = models.JSONField()
-    country = models.JSONField()
+    director = models.CharField(max_length=1000)
+    country = models.CharField(max_length=1000)
     imdb_rating = models.DecimalField(max_digits=3, decimal_places=1)
 
 
 class Still(models.Model):
-    # imdb_id = models.CharField(max_length=20)
     imdb_id = models.ForeignKey(
         Movie, on_delete=models.CASCADE, related_name='stills')
     image_url = models.CharField(max_length=1000)
